7a0d3a09b8af49f813b6f70ce2893c18f620e0be
Luke Arp <hardassgrannyslayer@gmail.com>
2016-09-06 21:55:57 -0400

    More messing around with the F-Rigid-Bodies project.  Added a basic jump, and logic to land on Sprites that are closest to the bottom of the player.

---
+++ F-Rigid-Bodies/Assets/Scripts/CameraFollowPlayer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollowPlayer : MonoBehaviour {
+    private Vector3 playerPosition;
+    private Vector3 thisCameraPosition;
+    public float screenBuffer;
+	// Use this for initialization
+	void Start () {
+        thisCameraPosition = GetComponent<Camera>().transform.position;
+        screenBuffer = 15f;
+        playerPosition = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>().position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        playerPosition = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>().position;
+        thisCameraPosition.y = playerPosition.y;
+        thisCameraPosition.x = playerPosition.x;
+        GetComponent<Camera>().transform.position = thisCameraPosition;
+    }
+}
--- F-Rigid-Bodies/Assets/Scripts/Player.cs
+++ F-Rigid-Bodies/Assets/Scripts/Player.cs
@@ -1,25 +1,48 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 using System.Collections;
 using System.Collections.Generic;
 
-public class Player : MonoBehaviour {    
+public class Player : MonoBehaviour {
+    /*  Player Position Variables */
     private Transform playerTransform;
     private SpriteRenderer playerRenderer;
-    private GameObject[] worldObjects;
     private Vector3 playerExtents;
-    public float fallSpeed = 0.2f;
-    public float moveSpeed = 0.1f;
-    private bool overSomething;
     private Vector3 position;
+    private float beforeJumpY;
+    /*  Player Position Variables */
+    /*     Player State Variables */
+    private bool overSomething;
+    private bool jumping;
+    private bool decending;
+    /*     Player State Variables */
+    /*     Speed/Max Variables    */
+    public float fallSpeed;
+    public float moveSpeed;
+    public float lowestYPoint;
+    public float jumpSpeed;
+    public float jumpMaxHeight;
+    private float timeJumpPress;
+    private float jumpForce;
+    /*     Speed/Max Variables    */
+    /*     World Variables        */
+    private float yOfWorld;
+    private GameObject[] worldObjects;
+    /*     World Variables        */
     void Awake ()
     {
         worldObjects = GameObject.FindGameObjectsWithTag("World");
         playerTransform = GetComponent<Transform>();
         playerRenderer = GetComponent<SpriteRenderer>();
+        fallSpeed = 6.85f;
+        moveSpeed = 6.0f;
+        jumpSpeed = 8f;
+        jumpMaxHeight = 10f;
+        lowestYPoint = -25f;
+        jumpForce = 1f;
     }
 	// Use this for initialization
-	void Start () {
-        Debug.Log("World Y position " + worldObjects[0].transform.position.y.ToString());
+	void Start () {        
         playerExtents = playerRenderer.bounds.extents;
         position = GetComponent<Transform>().position;
         Debug.Log("Player Transform position" + playerTransform.position.ToString());
@@ -32,43 +55,90 @@ public class Player : MonoBehaviour {
         overSomething = false;
         if (Input.GetKey(KeyCode.A))
         {
-            position.x -= moveSpeed;
+            position.x -= moveSpeed * Time.fixedDeltaTime;
         }
         if (Input.GetKey(KeyCode.D))
         {
-            position.x += moveSpeed;
+            position.x += moveSpeed * Time.fixedDeltaTime; 
         }
         if (Input.GetKey(KeyCode.W))
         {
-            position.y += moveSpeed;
+            position.y += moveSpeed * Time.fixedDeltaTime;
         }
+        if (Input.GetKeyDown(KeyCode.Space) && jumping == false && decending == false)
+        {
+            beforeJumpY = position.y;
+            jumping = true;
+            Debug.Log("beforeJumpY " + beforeJumpY);
+            Debug.Log("Max Y from Jump: " + (beforeJumpY + jumpMaxHeight));
+        }
+        if (Input.GetKeyUp(KeyCode.Space) || (position.y >= (beforeJumpY + (jumpMaxHeight/jumpForce)) && jumping == true))
+        {
+            jumping = false;
+            decending = true;
+            Debug.Log("Space Key Up");
+        }        
+        if (jumping == true)
+        {
+            Jump();
+        }
+        if (position.y < lowestYPoint)
+        {
+            Debug.Log("You Died");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+        float currentYofWorld = 0f;        
         foreach (GameObject world in worldObjects)
         {
-            if (position.x - playerExtents.x < world.transform.position.x + world.GetComponent<SpriteRenderer>().bounds.extents.x && position.x + playerExtents.x > world.transform.position.x - world.GetComponent<SpriteRenderer>().bounds.extents.x)
+            if (position.x - playerExtents.x < world.transform.position.x + world.GetComponent<SpriteRenderer>().bounds.extents.x && position.x + playerExtents.x > world.transform.position.x - world.GetComponent<SpriteRenderer>().bounds.extents.x && position.y - playerExtents.y >= world.transform.position.y + world.GetComponent<SpriteRenderer>().bounds.extents.y)
             {
+                if(world.GetComponent<SpriteRenderer>().bounds.extents.y + world.transform.position.y > currentYofWorld || currentYofWorld == 0f)
+                {
+                    currentYofWorld += world.GetComponent<SpriteRenderer>().bounds.extents.y + world.transform.position.y;
+                }
                 Debug.Log("I'm over something");
-                overSomething = true;
-                Fall(world.GetComponent<SpriteRenderer>().bounds.extents.y, world.transform.position.y);             
+                overSomething = true;                             
             }
         }
-        if(overSomething == false)
+        if (jumping == false)
         {
-            Debug.Log("Death You Fall");
+            Fall(currentYofWorld);
         }
+        Debug.Log("jumping Bool Value: " + jumping);
+        Debug.Log("decending Bool Value " + decending); 
 	}
 
-    void Fall(float worldYExtent, float worldYPosition)
+    void Fall(float worldY)
     {
-        if(worldYExtent + worldYPosition < position.y - playerExtents.y)
+        Debug.Log("WorldY Value: " + worldY.ToString());
+        if (position.y - playerExtents.y != worldY || overSomething == false)
         {
-            if ((position.y - playerExtents.y) - fallSpeed < worldYExtent + worldYPosition)
+            position.y -= fallSpeed * Time.fixedDeltaTime;
+            decending = true;
+        }            
+        if (overSomething == true)
+        {
+            if (position.y - playerExtents.y < worldY)
             {
-                position.y = playerExtents.y + worldYExtent + worldYPosition;
+                position.y = worldY + playerExtents.y;
+                decending = false;
+                Debug.Log("Decend Condition Met False");
+            }
+            /*
+            if ((position.y - playerExtents.y) - fallSpeed < worldY)
+            {
+                position.y = playerExtents.y + worldY;
             }
             else
             {
-                position.y -= fallSpeed;
+                position.y -= fallSpeed * Time.fixedDeltaTime;
             }
+            */
         }
+    }
+
+    void Jump()
+    {  
+        position.y += jumpSpeed * Time.fixedDeltaTime;
     }
 }
