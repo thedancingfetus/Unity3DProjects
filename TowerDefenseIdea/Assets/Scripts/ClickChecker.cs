5cbe37a22cf79f99880806707d04523d7b333cff
Luke Arp <hardassgrannyslayer@gmail.com>
2016-09-14 00:30:51 -0400

    Added a Tower Prefab, and added a FixedUpdate() loop for adding to the ControllerScript array.  This seems to have fixed an issue I was having were sometimes 2 towers would be locked to the same transform position.

--- TowerDefenseIdea/Assets/Scripts/ClickChecker.cs
+++ TowerDefenseIdea/Assets/Scripts/ClickChecker.cs
@@ -6,7 +6,7 @@ public class ClickChecker : MonoBehaviour {
     public struct clickObject
     {
         public float distance;
-        public TowerScript thing;
+        public GameObject thing;
     }
     private Vector3 mousePostion;
     public List<clickObject> clickedObject;
@@ -18,12 +18,12 @@ public class ClickChecker : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
+        Debug.Log(clickedObject.Count);
         if (clickedObject.Count > 0)
         {
-            Debug.Log(clickedObject.Count);
             clickedObject.Sort((obj1, obj2) => obj1.distance.CompareTo(obj2.distance));
-            clickedObject.ToArray()[0].thing.GetComponent<TowerScript>().clickedOnMe = true;
-        }
+            clickedObject[0].thing.GetComponent<TowerScript>().clickedOnMe = true;
+        }          
         clickedObject.Clear();
     }
 }
--- TowerDefenseIdea/Assets/Scripts/TowerScript.cs
+++ TowerDefenseIdea/Assets/Scripts/TowerScript.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEditor;
 using System.Collections;
 
 public class TowerScript : MonoBehaviour {
@@ -8,31 +9,57 @@ public class TowerScript : MonoBehaviour {
     public float towerPostionY;
     public float towerPostionZ;
     public bool clickedOnMe;
+    private bool updateList;
+    private bool createNewTower;
+    public PrefabUtility prefab;
+    public ClickChecker controllerObject;
 	// Use this for initialization
 	void Start () {
         towerPostion = GetComponent<Transform>().position;
         towerPostion.z = towerPostion.y / 100;
         GetComponent<Transform>().position = towerPostion;
         towerExtents = GetComponent<SpriteRenderer>().bounds.extents;
+        controllerObject = GameObject.Find("ControllerObject").GetComponent<ClickChecker>();
         clickedOnMe = false;
-	}
+        updateList = false;
+        createNewTower = false;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        GetComponent<Transform>().position = towerPostion;
+        GetComponent<Transform>().position = towerPostion;        
         mousePostion = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        if(Input.GetMouseButtonDown(0) && mousePostion.x > towerPostion.x - towerExtents.x && mousePostion.x < towerPostion.x + towerExtents.x && mousePostion.y > towerPostion.y - towerExtents.y && mousePostion.y < towerPostion.y + towerExtents.y)
+        if (Input.GetMouseButtonDown(0) && mousePostion.x > towerPostion.x - towerExtents.x && mousePostion.x < towerPostion.x + towerExtents.x && mousePostion.y > towerPostion.y - towerExtents.y && mousePostion.y < towerPostion.y + towerExtents.y)
         {
-            GameObject.Find("ControllerObject").GetComponent<ClickChecker>().clickedObject.Add(new ClickChecker.clickObject() { distance = Vector3.Distance(mousePostion ,towerPostion) , thing = GetComponent<TowerScript>() });        
+            updateList = true;
         }
         if (Input.GetMouseButton(0) && clickedOnMe == true)
         {
-            towerPostion.z = mousePostion.y / 100;
-            towerPostion = new Vector3(mousePostion.x, mousePostion.y, towerPostion.z);
+            Debug.Log(GetComponent<Transform>().localScale.normalized);
+            towerPostionZ = mousePostion.y / 100;
+            towerPostion = new Vector3(mousePostion.x, mousePostion.y, towerPostionZ);
         }
         if (Input.GetMouseButtonUp(0))
         {
             clickedOnMe = false;
+        }
+        if (Input.GetMouseButtonDown(1) && mousePostion.x > towerPostion.x - towerExtents.x && mousePostion.x < towerPostion.x + towerExtents.x && mousePostion.y > towerPostion.y - towerExtents.y && mousePostion.y < towerPostion.y + towerExtents.y)
+        {
+            createNewTower = true;
+        }
+    }
+
+    void FixedUpdate ()
+    {
+        if (updateList == true)
+        {
+            controllerObject.clickedObject.Add(new ClickChecker.clickObject() { distance = Vector3.Distance(mousePostion, towerPostion), thing = gameObject });
+            updateList = false;
+        } 
+        if (createNewTower == true)
+        {
+            PrefabUtility.InstantiatePrefab(PrefabUtility.GetPrefabParent(gameObject));
+            createNewTower = false;
         }
     }
 }
